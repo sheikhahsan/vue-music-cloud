e9dae7f1bbd1fbbc7173c66840a44de6de8e4cf8
sheikhahsan <ravianfastian@gmail.com>
2017-09-16 03:36:32 +0500

    Player.vue added

---
+++ src/components/Player.vue
@@ -0,0 +1,86 @@
+<template>
+<div id ="player">
+	<hr>
+	<div class="ui sixteen column grid">
+		<!-- <div class="row"> -->
+			<div class="one wide column">
+	    		<img class="ui mini image" src="/static/images/song1.png">
+			</div>
+			<div class="two wide column">
+				<div class="ui text">Quavo</div>
+				<div class="ui sa-bold text">Stars In The Ceiling</div>
+			</div>
+			<div class="column">
+				<i class="large step backward icon"></i>
+			</div>
+			<div class="column">
+				<i class="large play icon"></i>
+			</div>
+			<div class="column">
+				<i class="large step forward icon"></i>
+			</div>
+			<div class="six wide column">
+			</div>
+			<div class="four wide right floated column">
+				<div class="ui twelve column grid">
+					<div class="five wide column">
+						<div class="ui sa-bod text icon">
+						0:07/3:39
+						</div>
+					</div>
+					<div class="two wide column">
+						<i class="large random icon"></i>
+					</div>
+					<div class="two wide column">
+						<i class="large retweet icon"></i>
+					</div>
+					<div class="two wide column">
+						<i class="large empty star icon"></i>
+					</div>
+					<div class="two wide column">
+						<i class="big volume up icon"></i>
+					</div>
+				</div>
+			</div>
+
+		<!-- </div> -->
+    </div>
+</div>
+</template>
+
+
+<style scoped>
+#player {
+    bottom: 0;
+    color: white;
+    left: 0;
+    position: fixed;
+    height: 70px;   
+    width: 100%; 
+}
+
+.ui.mini.image{
+	width: 50px;
+	height: 50px;
+	margin-left: 10px;
+}
+
+.large.icon{
+	margin-top: 14px;
+}
+
+.big.icon{
+	margin-top: 10px;
+}
+
+.icon.text{
+	margin-top: 15px;
+	font-weight: bold;
+	font-size: 15px;
+}
+
+.sa-bold{
+	font-weight: bold;
+}
+
+</style>
---
+++ src/components/Trending.vue
@@ -0,0 +1,96 @@
+<template>
+	<div class="ui inverted vertical segment">
+		<div class="ui container">
+<!-- 			<table class="ui inverted table">
+			  <thead>
+			    <tr>
+			      <th>Song</th>
+			      <th>Artist</th>
+			      <th>Notes</th>
+			    </tr>
+			  </thead>
+			  <tbody>
+			    <tr>
+			      <td>John</td>
+			      <td>Approved</td>
+			      <td>None</td>
+			    </tr>
+			    <tr>
+			      <td>Jamie</td>
+			      <td>Approved</td>
+			      <td>Requires call</td>
+			    </tr>
+			    <tr>
+			      <td>Jill</td>
+			      <td>Denied</td>
+			      <td>None</td>
+			    </tr>
+			  </tbody>
+			  <tfoot>
+			    <tr><th>3 People</th>
+			    <th>2 Approved</th>
+			    <th></th>
+			  </tr></tfoot>
+			</table> -->
+
+			<div class="ui five stackable cards">
+			  <div class="card">
+			    <div class="image">
+			      <img src="/static/images/elliot.jpg">
+			    </div>
+				<div class="content">
+					<a class="header" href="#">Steve Jobes</a>
+					<div class="meta">
+						<a>by DJ Technics House</a>
+					</div>
+				</div>
+			  </div>
+			  <div class="card">
+			    <div class="image">
+			      <img src="/static/images/helen.jpg">
+			    </div>
+			  </div>
+			  <div class="card">
+			    <div class="image">
+			      <img src="/static/images/jenny.jpg">
+			    </div>
+			  </div>
+			  <div class="card">
+			    <div class="image">
+			      <img src="/static/images/veronika.jpg">
+			    </div>
+			  </div>
+			  <div class="card">
+			    <div class="image">
+			      <img src="/static/images/stevie.jpg">
+			    </div>
+			  </div>
+			  <div class="card">
+			    <div class="image">
+			      <img src="/static/images/steve.jpg">
+			    </div>
+			  </div>
+			</div>
+		</div>
+		<player id="player"></player>
+	</div>
+</template>
+
+<script>
+import Player from './Player'	
+export default{
+	name: 'Trending',
+	components:{
+		Player
+	}
+}
+</script>
+
+<style scoped>
+
+  .segment {
+    min-height: 700px;
+    padding: 1em 0em;
+  }
+
+</style>
