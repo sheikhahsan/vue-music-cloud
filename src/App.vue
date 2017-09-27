86c52cefdaaccbdfb754701959fabe03c17a919a
sheikhahsan <ravianfastian@gmail.com>
2017-09-28 00:02:57 +0500

    SIGN UP modal added

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,43 @@
 <template>
   <div id="app">
 
+
+	<div class="ui mini modal">
+		<div class="header">
+			Create an Account
+		</div>
+		<div class="content">
+			<div class="fluid ui vertical buttons">
+				<button class="ui facebook button">
+					<i class="facebook icon"></i>
+					Join with Facebook
+				</button>
+				<div class="ui fitted hidden divider"></div>
+				<div class="ui fitted hidden divider"></div>
+				<div class="ui fitted hidden divider"></div>
+				<div class="ui fitted hidden divider"></div>
+				<button class="ui google plus fluid button">
+					<i class="google icon"></i>
+					Join with Google
+				</button>
+			</div>
+			<div class="ui horizontal divider">
+				Or
+			</div>
+			<div class="ui form">
+				<div class="field">
+					<input type="email" placeholder="Email">
+				</div>
+				<div class="field">
+					<input type="password" placeholder="Password">
+				</div>
+			</div>
+		</div>
+		<div class="actions">
+				<div class="ui secondary basic button">SIGN UP</div>
+		</div>
+	</div>
+
    	<!-- Sidebar Menu -->
   	<div :class="{'ui vertical inverted sidebar menu': true, visible: elementVisible.sidebar}">
 		<a id="sidebar-icon" class="toc item"  @click="toggleSidebar">
@@ -26,7 +63,7 @@
       			:class='{active: currentPage==key}'>{{key}}</a>
       			<div class="right item">
 					<a class="ui inverted button">Log in</a>
-					<a class="ui inverted button">Sign Up</a>
+					<a class="ui inverted button" @click="openModal">Sign Up</a>
       			</div>
 		  	</div>
 		</div>
@@ -64,6 +101,11 @@ export default {
 	    goToRoute (name,path) {
 	    	this.currentPage = name;
 	    	this.$router.push(path);
+	    },
+	    openModal () {
+	    	$('.ui.modal')
+			  .modal('show')
+			;
 	    }
 	}
 }
